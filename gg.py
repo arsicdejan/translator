900c279eb4ab6250b8214db1426140779ee94211
arsicdejan <arsicdejan996@gmail.com>
2021-03-05 10:41:26 +0100

    adding starting project

---
+++ gg.py
@@ -0,0 +1,21 @@
+from translate import Translator
+translator = Translator(to_lang='ja')
+try:
+    with open('./test.txt', mode='r+') as file_to_translate:
+        text_to_translate = (file_to_translate.read())
+        translation = translator.translate(text_to_translate)
+    with open('./translated.txt', mode='a') as translated:
+        translated.write(translation)
+except FileNotFoundError as e:
+    print('file not found')
+
+
+
+
+
+
+
+
+
+
+
